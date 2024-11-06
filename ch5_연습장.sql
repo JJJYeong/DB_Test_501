133cb92e61196df685d7b5f9e5c1f6ac58e3584c
이상용 <lsy3709@naver.com>
2024-11-06 15:24:00 +0900

    6교시 5장 커밋 정리

--- ch5_연습장.sql
+++ ch5_연습장.sql
@@ -76,3 +76,48 @@ where sal between 2000 and 3000;
 -- 반대 경우 
 select * from emp 
 where sal not between 2000 and 3000;
+
+-- 실습 5-20
+-- %, 0문자 이상
+-- S 로시작하는 모든 문자열 검색. 
+select * from emp 
+where ename like 'S%';
+
+-- 실습 5-21
+-- _ , 언더바는 하나의 문자를 의미. 
+select * from emp 
+where ename like '__L%';
+
+
+-- 실습 5-22
+select * from emp 
+where ename like '%AM%';
+
+
+-- 실습 5-23
+select * from emp 
+where ename NOT like '%AM%';
+
+-- 실습 5-24
+select * from emp;
+
+-- 실습 5-25
+select * from emp
+WHERE COMM = null;
+
+-- 실습 5-26
+-- is null 로 확인 다시 해보기. 
+select * from emp
+WHERE comm is null;
+
+-- 실습 5-27
+-- is not null 로 확인 다시 해보기.
+-- mgr , 해당 사원의 직속상관의 사원 번호 의미. 
+select * from emp
+WHERE mgr is not null;
+
+-- 실습 5-33
+select empno, ename, sal, deptno from emp where deptno =10
+union
+select sal, job, deptno, sal from emp where deptno =10;
+
