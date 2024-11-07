d8b4a4017b62c2bc2d3c19fc52fc7fb25e44e4a8
이상용 <lsy3709@naver.com>
2024-11-07 14:21:22 +0900

    1107, 5교시 7장 다중행함수 count 함수 확인중

--- ch7_연습장.sql
+++ ch7_연습장.sql
@@ -11,5 +11,49 @@ select sum(comm) from emp;
 
 -- 실습 7-4
 -- distinct , all 
+select sal from emp;
 select sum(distinct sal), sum(all sal), sum(sal) from emp;
 
+
+-- 실습 7-5
+select count(*) from emp;
+
+-- 실습 7-6 
+
+SELECT COUNT(*) 
+
+  FROM EMP 
+
+ WHERE DEPTNO = 30; 
+
+ 
+
+-- 실습 7-7 
+
+SELECT COUNT(DISTINCT SAL), 
+
+       COUNT(ALL SAL), 
+
+       COUNT(SAL) 
+
+  FROM EMP; 
+
+ 
+
+-- 실습 7-8 
+
+SELECT COUNT(COMM) 
+
+  FROM EMP; 
+
+ 
+
+-- 실습 7-9 
+
+SELECT COUNT(COMM) 
+
+  FROM EMP 
+
+ WHERE COMM IS NOT NULL; 
+
+ 
