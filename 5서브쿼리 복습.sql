83db8e08ee0b2729feaea627c526e58a78e09494
이상용 <lsy3709@naver.com>
2024-11-11 10:18:26 +0900

    1111,2교시 DML 복습

--- 5서브쿼리 복습.sql
+++ 5서브쿼리 복습.sql
@@ -1,234 +1,92 @@
 -- 서브쿼리  
-
- 
-
--- 팝 직원의 고용일보다 이후에 고용된 직원들  
-
- 
-
+-- SCOTT 직원의 고용일보다 이후에 고용된 직원들  
 -- 단일행 서브쿼리는 서브쿼리 결과가 한줄만 나와야 된다.  
 
- 
-
-select ename 이름, hiredate 고용일자  
-
- 
-
+ select ename 이름, hiredate 고용일자  
 from emp  
-
- 
-
 where hiredate > ( select hiredate   
-
- 
-
                     from emp   
-
- 
-
                     where ename = 'SCOTT');  
 
  
 
 -- 집계함수 서브쿼리  
-
- 
-
 select ename 이름, job 직종, sal 급여  
-
- 
-
 from emp  
-
- 
-
 where sal = (select min(sal) from emp);   
 
  
 
 -- 예제 풀기 1  
-
- 
-
 SELECT ename 이름, sal 급여  
-
- 
-
 FROM emp  
-
- 
-
 WHERE sal > ( SELECT sal FROM emp WHERE ename = 'SCOTT');  
 
  
 
 -- 예제 풀기 2  
 
- 
-
 SELECT empno 직원번호, ename 이름, deptno 부서번호, sal 급여  
-
- 
-
 FROM emp  
-
- 
-
 WHERE deptno = ( SELECT deptno FROM emp WHERE ename = 'SCOTT')  
-
- 
-
-AND sal > ( SELECT sal FROM emp WHERE ename = 'SMITH');  
+AND sal > ( SELECT sal FROM emp WHERE ename = 'SMITH');
+SELECT sal FROM emp WHERE ename = 'SMITH';
 
  
 
 -- 예제 풀기 3  
-
- 
-
 SELECT ename 이름, sal 급여, mgr 매니저  
-
- 
-
 FROM emp  
-
- 
-
 WHERE mgr = ( SELECT empno FROM emp WHERE ename = 'SCOTT');  
+select * from emp;
 
- 
 
 -- 예제 풀기 4  
-
- 
-
-SELECT *   
-
- 
-
+ SELECT *   
 FROM emp  
-
- 
-
 WHERE deptno = ( SELECT deptno FROM dept WHERE deptno = 10 );  
-
- 
-
-  
-
- 
 
 -- 다중행 서브쿼리  
 
- 
 
 select min(sal)  
-
- 
-
 from emp  
-
- 
-
 group by deptno;  
 
- 
-
 -- 부서별 최소급여와 같은 급여를 받는 직원들  
-
- 
-
 select empno, ename  
-
- 
-
 from emp  
-
- 
-
 where sal IN (select min(sal)  
-
- 
-
                 from emp  
-
- 
-
                 group by deptno);  
 
  
 
 -- 다중열 ANY연산자 : 서브쿼리 결과중 하나라도 참이면 검색됨               
-
- 
-
 SELECT empno, ename, job, sal  
-
- 
-
 FROM emp  
-
- 
-
 WHERE sal  < ANY  
-
- 
-
-                    ( SELECT sal  
-
- 
-
-                      FROM emp  
-
- 
-
+                   ( SELECT sal  
+                      FROM emp
                       WHERE job = 'MANAGER')  
-
- 
-
-AND job <> 'MANAGER'  
-
- 
-
+AND job <> 'MANAGER' 
 ORDER BY sal DESC;  
+ SELECT sal  
+                      FROM emp
+                      WHERE job = 'MANAGER';
 
  
 
 -- ALL: 서브쿼리 결과가 모두 참이여야지 검색됨  
 
- 
-
 SELECT empno, ename, job, sal  
-
- 
-
 FROM emp  
-
- 
-
 WHERE sal  < ALL  
-
- 
-
                     ( SELECT sal  
-
- 
-
                       FROM emp  
-
- 
-
                       WHERE job = 'MANAGER')  
-
- 
-
 AND job <> 'MANAGER'  
-
- 
-
 ORDER BY sal DESC;  
-
- 
 
   
 
@@ -236,39 +94,18 @@ ORDER BY sal DESC;
 
 -- 예제 1  
 
- 
-
-SELECT empno, ename, job  직종, sal 급여  
-
- 
-
+ SELECT empno, ename, job  직종, sal 급여  
 FROM emp  
-
- 
-
 WHERE mgr IN (SELECT mgr FROM emp WHERE deptno = 20)  
-
- 
-
-    AND deptno != 20;  
-
+ AND deptno != 20;  
  
 
 -- 예제 2    
 
- 
-
-SELECT empno, ename, job  직종, sal 급여  
-
- 
-
+ SELECT empno, ename, job  직종, sal 급여  
 FROM emp  
-
- 
-
 WHERE sal < ANY(SELECT sal FROM emp WHERE job = 'MANAGER');  
 
- 
 
 -- 예제 3  
 
@@ -295,29 +132,11 @@ WHERE sal < ALL(SELECT sal FROM emp WHERE job = 'MANAGER');
  
 
 -- 브루스와 같은 직종및 같은 상사를 가진 직원들  
-
- 
-
-SELECT empno, ename, job, sal, mgr  
-
- 
-
+ SELECT empno, ename, job, sal, mgr  
 FROM emp  
-
- 
-
 WHERE (mgr, job) IN  (SELECT mgr, job  
-
- 
-
                                 FROM emp  
-
- 
-
                                 WHERE ename = 'SCOTT')  
-
- 
-
 AND ename <> 'SCOTT';  
 
  
@@ -328,52 +147,21 @@ AND ename <> 'SCOTT';
 
 -- 집합연산자 (UNION, INTERSECT, MINUS)  
 
- 
-
 --UNION (107 + 10) = 117 인데 총 115줄  
 
- 
-
 select empno, job  
-
- 
-
 from emp  
-
- 
-
 union  
-
- 
-
 select deptno, dname  
-
- 
-
 from dept;  
 
  
 
 --INTERSECT (중복되는 값만 나옴)  
-
- 
-
 select empno, job  
-
- 
-
 from emp  
-
- 
-
 INTERSECT  
-
- 
-
 select deptno, dname  
-
- 
-
 from dept;  
 
  
@@ -383,15 +171,9 @@ from dept;
  
 
 select empno, job  
-
- 
-
 from emp  
 
- 
-
 MINUS  
-
  
 
 select empno, job  
---
+++ 6 DML 복습.sql
@@ -0,0 +1,107 @@
+-- 입력 INSERT 
+
+-- 테이블 열의 데이터타입 간단히 보기 
+
+DESC dept; 
+
+INSERT INTO dept(deptno,dname,loc) 
+
+VALUES(11, '게임부', '부산서면'); 
+
+-- 열이름 생략할때(모든 열을 다입력해야함) 
+
+INSERT INTO dept 
+
+VALUES(12, '게임부2', '부산서면2'); 
+
+-- 널값이 입력될때(입력을 하지 않으면 널) 
+
+INSERT INTO dept(deptno,dname) 
+
+VALUES(13, '게임부3', ); 
+
+ 
+
+--직원테이블 입력 
+
+DESC emp; 
+
+INSERT INTO emp(empno, ename, job, mgr, hiredate, sal, comm, deptno) 
+
+VALUES(2070,'이상용','TEA',null,sysdate, 1000, 500, null); 
+
+-- 예제 1 
+
+INSERT INTO dept VALUES(14, '게임부4', '부산서면4'); 
+
+INSERT INTO dept VALUES(15, '게임부5', '부산서면5');
+
+INSERT INTO dept VALUES(16, '게임부6', '부산서면6');
+
+ 
+
+-- 수정 UPDATE (주의 :where이 없으면 모든행이 업데이트 됨) 
+
+-- 음악부서의 매니저와 로케이션 수정하기 
+
+UPDATE dept 
+
+SET dname = '수정부서1', loc = '수정지역1' 
+
+WHERE deptno = 11; 
+
+-- 예제 부서번호 150에서 200번까지 매니저ID를 100으로 수정하기 
+
+UPDATE dept 
+
+SET dname = '수정부서2', loc = '수정지역2' 
+
+WHERE deptno between 10 and 12;  
+
+ 
+
+-- 삭제하기 DELETE 
+
+delete from dept 
+
+where dname = '게임부'; 
+
+delete from dept 
+
+where dname = '게임'; 
+
+-- 만약 삭제하는 데이터를 참조하는 테이블과 데이터가 있을경우 참조하는 데이터를 먼저 삭제해야 삭제가능 
+
+DELETE FROM emp where ename = '이상용'; 
+
+-- 삭제시 where절을 안적으면? : 전체가 적용되므로 주의 
+
+UPDATE emp SET eanme = '전체 변경 샘플 롤백 할 예정'; 
+
+ 
+
+-- 그전 상태로 복구(이전의 성공적 commit 상태로 돌아감) 
+
+rollback; 
+
+-- 입력 수정 삭제 연습하기 
+
+-- scott 계정 
+
+delete from sal; 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
