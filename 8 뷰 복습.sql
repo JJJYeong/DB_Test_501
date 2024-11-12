3692bd174432488bc82265ff5001ccd24835ce93
이상용 <lsy3709@naver.com>
2024-11-12 09:22:44 +0900

    1112,1교시 뷰 복습1차수정

--- 8 뷰 복습.sql
+++ 8 뷰 복습.sql
@@ -1,24 +1,20 @@
 -- 뷰 view 가상의 테이블 
-
 CREATE VIEW EMP_V2 
-
 AS 
-
-select employee_id 직원번호, first_name 이름, last_name 성, email, hire_date, job_id 
-
-from employees; 
+select empno 직원번호, ename 이름, hiredate, job 
+from emp; 
 
  
 
 select * from emp_v2; 
 
-INSERT INTO emp_v2 VALUES(207, '길동','홍','HONG','2023-7-31','IT_PROG'); 
+INSERT INTO emp_v2 VALUES(1234,'이상용',sysdate,'강사'); 
 
 -- 뷰는 실제 물리적인 테이블이 아닌데 입력을 했음 => 실제로 뷰를 만든 테이블에 입력됨! 
 
-Delete from emp_v2 where 직원번호 = 207; 
+Delete from emp_v2 where 직원번호 = 1234; 
 
-select * from employees; 
+select * from emp; 
 
  
 
@@ -28,14 +24,14 @@ CREATE VIEW EMP_read(직원번호,이름,성,이메일,고용일자,직종)
 
 AS 
 
-select employee_id, first_name, last_name, email, hire_date, job_id 
+select empno, ename, hiredate, job 
 
-from employees with read only; 
+from emp with read only; 
 
  
 
 select * from emp_read; 
 
-Delete from emp_read where 직원번호 = 100; 
+Delete from emp_read where 직원번호 = 1234; 
 
 -- 읽기만 가능한 뷰는 DML(입력,수정,삭제)가 안됨.  
