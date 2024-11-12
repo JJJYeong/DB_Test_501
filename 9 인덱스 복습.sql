cd163567ffacafb3e7da808ace7219552794ab96
이상용 <lsy3709@naver.com>
2024-11-12 09:38:44 +0900

    1112,1교시 인덱스 복습 오타수정

--- 9 인덱스 복습.sql
+++ 9 인덱스 복습.sql
@@ -1,51 +1,33 @@
 -- 직원테이블의 인덱스 모음 
 
 SELECT  
-
       TABLE_NAME             테이블명, 
-
       INDEX_NAME             인덱스이름, 
-
       COLUMN_NAME            컬럼이름 
-
 FROM ALL_IND_COLUMNS  
-
-WHERE TABLE_NAME = 'EMPLOYEES'; 
+WHERE TABLE_NAME = 'EMP'; 
 
 -- 기본키, 유니크 열은 자동으로 인덱스가 생성됨 또한 기본키를 참조하는 외래키도 인덱스가 있음 
 
  
 
 -- 예제용 멤버 테이블 만들기 
-
+drop table members;
 CREATE TABLE members( 
-
     member_id NUMBER, 
-
     first_name VARCHAR2(100) NOT NULL, 
-
     last_name VARCHAR2(100) NOT NULL, 
-
     gender CHAR(1) NOT NULL, 
-
     dob DATE NOT NULL, 
-
     email VARCHAR2(255) NOT NULL, 
-
     PRIMARY KEY(member_id) --제약조건을 아래의 행에 따로 넣을때 괄호안에 열의 이름 입력 
-
 ); 
 
 SELECT  
-
       TABLE_NAME             테이블명, 
-
       INDEX_NAME             인덱스이름, 
-
       COLUMN_NAME            컬럼이름 
-
 FROM ALL_IND_COLUMNS  
-
 WHERE TABLE_NAME = 'MEMBERS'; --테이블명은 대문자 
 
  
@@ -65,11 +47,8 @@ where last_name = 'Harse';
 -- 보고서 확인 
 
 SELECT  
-
     PLAN_TABLE_OUTPUT  
-
 FROM  
-
     TABLE(DBMS_XPLAN.DISPLAY()); 
 
  
@@ -77,7 +56,6 @@ FROM
 --인덱스 만들기 멤버즈 테이블의 last_name열 , 이름은 members_last_name_i 
 
 CREATE INDEX members_last_name_i 
-
 ON members(last_name); 
 
  
