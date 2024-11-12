ae33a6595647dcd4b7802a498a9cc142888f8cce
이상용 <lsy3709@naver.com>
2024-11-12 10:03:43 +0900

    1112,2교시 14 제약조건

--- 11 로우넘 복습.sql
+++ 11 로우넘 복습.sql
@@ -7,13 +7,11 @@ from emp where ROWNUM <=5 ;
 -- 6번부터 10번까지? (로우넘은 1번부터 임시로 번호를 매기는거라 다른 조건으로는 검색안됨) 
 
 select ROWNUM, emp.* 
-
 from emp where ROWNUM BETWEEN 6 AND 10; 
 
 -- 테이블에 서브쿼리로 입력시 번호 ROWNUM이 있는 테이블 
 
 SELECT * FROM (select ROWNUM AS RN,emp.* from emp)   
-
 WHERE RN BETWEEN 6 AND 10; 
 
  
@@ -21,21 +19,15 @@ WHERE RN BETWEEN 6 AND 10;
 -- 정렬(order by) 이전에 ROWNUM 번호를 매기기 때문에 마찬가지로 
 
 SELECT ROWNUM, EMP.*  
-
 FROM EMP  
-
 WHERE ROWNUM <= 5 ORDER BY ENAME; 
 
 -- 정렬한 테이블을 서브쿼리에 넣은 다음에 로우넘으로 번호 
 
 SELECT ROWNUM, A.* 
-
 FROM ( 
-
     SELECT EMPNO, ENAME FROM EMP ORDER BY ENAME 
-
 ) A 
-
 WHERE ROWNUM <= 5; 
 
  
